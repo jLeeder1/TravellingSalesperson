1816c3da533238a55e9d43661ad716caedbe49e8
Josh Leeder <joshleeder1@gmail.com>
2020-10-03 12:50:23 +0100

    Changed from HashSet to List

--- TravellingSalespersonProj/TravellingSalespersonProj/Program.cs
+++ TravellingSalespersonProj/TravellingSalespersonProj/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TravellingSalespersonProj
 {
@@ -12,13 +13,34 @@ namespace TravellingSalespersonProj
         static void Main(string[] args)
         {
             // Always true for now as always using default graph
+            /*
             GraphGenerator graphGenerator = new GraphGenerator(true);
             Graph graph = graphGenerator.GenerateGraph();
+            RouteGenerator routeGenerator = new RouteGenerator();
 
             foreach (KeyValuePair<string, Node> entry in graph.NodesInCity)
             {
                 Console.WriteLine($"Starting Node: {entry.Key}{System.Environment.NewLine}{entry.Value.PrintAllDestinationsAndWeights()}");
             }
+
+            routeGenerator.GenerateRoutes(4, "A");
+            */
+            HashSet<string[]> test = new HashSet<string[]>();
+
+            string[] route = new string[] { "A", "B", "20" };
+            string[] routeTwo = new string[] { "A", "B", "20" };
+
+            test.Add(route);
+            test.Add(routeTwo);
+
+            foreach(string[] current in test)
+            {
+                for(int i = 0; i < current.Length; i++)
+                {
+                    Console.WriteLine(current[i]);
+                }
+            }
+            Console.WriteLine(test.Count());
         }
 
     }
--- TravellingSalespersonProj/TravellingSalespersonProj/RouteGenerator.cs
+++ TravellingSalespersonProj/TravellingSalespersonProj/RouteGenerator.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TravellingSalespersonProj
 {
     public class RouteGenerator
     {
-        public HashSet<string[]> GenerateRoutes(int numberOfNodes, Node startingNode)
+        public List<string[]> GenerateRoutes(int numberOfNodes, string startingNode)
         {
             long numberOfRoutes = CalculateMaximumAmountOfRoutes(numberOfNodes);
+            List<string> possibleNodes = CreateListOfPossibleNodes(numberOfNodes);
 
-            HashSet<string[]> routes = CalculateRoutes(startingNode, numberOfRoutes);
+            List<string[]> routes = CalculateRoutes(startingNode, numberOfRoutes, possibleNodes);
 
             return null;
         }
+
+        // Pass in or calculate all possible nodes (probably has to be a list)
+        // add starting node to beginning of arrayToReturn
+        // remove random element of list and add to array
+        // Add starting node to end of arrayToReturn
 
         private long CalculateMaximumAmountOfRoutes(int numberOfNodes)
         {
@@ -28,10 +35,42 @@ namespace TravellingSalespersonProj
             return numberOfRoutes - 1;
         }
 
-        private HashSet<string[]> CalculateRoutes(Node startingNode, long numberOfRoutes)
+        private List<string> CreateListOfPossibleNodes(int numberOfNodes)
         {
-            HashSet<string[]> routes = new HashSet<string[]>();
+            List<string> possibleNodes = new List<string>();
 
+            for(int index = 1; index <= numberOfNodes; index++)
+            {
+                possibleNodes.Add(ConvertIntToString.Convert(index));
+            }
+
+            return possibleNodes;
+        }
+
+        private List<string[]> CalculateRoutes(string startingNode, long numberOfRoutes, List<string> possibleNodes)
+        {
+            List<string[]> routes = new List<string[]>();
+
+            // Array for single route
+            string[] route = new string[possibleNodes.Count+1];
+            route[0] = possibleNodes.ElementAt(0);
+            possibleNodes.RemoveAt(0);
+            Random random = new Random();
+
+            while(routes.Count < numberOfRoutes)
+            {/*
+                for(int index = 0; index < possibleNodes)
+                {
+                    int randomNum = random.Next(possibleNodes.Count);
+                    if (possibleNodes.ElementAt(randomNum) != null)
+                    {
+                        route[route.Length - 1] = possibleNodes.ElementAt(randomNum);
+                        possibleNodes.RemoveAt(randomNum);
+                    }
+
+                    routes.Add(route);
+                } */
+            }
 
             return routes;
         }
