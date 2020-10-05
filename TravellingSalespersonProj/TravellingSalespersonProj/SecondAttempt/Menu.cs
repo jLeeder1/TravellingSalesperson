1aae2cc27400fb190214380b2e891ae24412f8f7
Josh Leeder <joshleeder1@gmail.com>
2020-10-05 12:21:07 +0100

    Tasks 1 to 4 completed

--- TravellingSalespersonProj/TravellingSalespersonProj/SecondAttempt/Menu.cs
+++ TravellingSalespersonProj/TravellingSalespersonProj/SecondAttempt/Menu.cs
@@ -6,6 +6,15 @@ namespace TravellingSalespersonProj.SecondAttempt
 {
     public class Menu
     {
+        RandomRouteGenerator randomRouteGenerator;
+        RouteEvaluator routeEvaluator;
+
+        public Menu()
+        {
+            this.randomRouteGenerator = new RandomRouteGenerator();
+            this.routeEvaluator = new RouteEvaluator();
+        }
+
         public void RunMenu()
         {
             Console.WriteLine($"Please enter a number from the choices below:{System.Environment.NewLine}");
@@ -46,6 +55,22 @@ namespace TravellingSalespersonProj.SecondAttempt
         {
             Graph graph = new Graph();
             graph.RunDefaultGraphSetUp();
+            int[] route = randomRouteGenerator.GenerateSingleRandomRoute(graph.GraphOfNodes.Count, 0);
+            float costOfRoute = routeEvaluator.CalculateCostOfSingleRoute(route, graph);
+            PrintRouteAndCalculation(route, costOfRoute);
+            RunMenu();
+        }
+
+        private void PrintRouteAndCalculation(int[] route, float costOfRoute)
+        {
+            string routePath = String.Empty;
+
+            foreach(int nodeId in route)
+            {
+                routePath = $"{routePath} {nodeId.ToString()}";
+            }
+
+            Console.WriteLine($"The route taken was {routePath} with a cost of {costOfRoute}");
         }
     }
 }
