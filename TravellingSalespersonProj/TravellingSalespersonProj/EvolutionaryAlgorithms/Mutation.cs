bad3c189e0f7c175342c606c5a80376ac093494c
Josh Leeder <joshleeder1@gmail.com>
2020-11-07 12:45:16 +0000

    Mutation added and working

---
+++ TravellingSalespersonProj/TravellingSalespersonProj/EvolutionaryAlgorithms/Mutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TravellingSalespersonProj.LocalSearchTutorial;
+
+namespace TravellingSalespersonProj.EvolutionaryAlgorithms
+{
+    public class Mutation
+    {
+        private readonly Random random = new Random();
+        private readonly double CHANCE_OF_MUTATION = 0.25;
+
+        public void SwapMutateIndividual(Route individual, int numberOfSwaps = 1)
+        {
+            // Random chance to mutate
+            if(random.Next(1) > CHANCE_OF_MUTATION)
+            {
+                return;
+            }
+
+            int[] updatedRoute = individual.RouteIds;
+
+            for(int index = 1; index <= numberOfSwaps; index++)
+            {
+                int firstIndex = random.Next(1, updatedRoute.Length - 2);
+                int secondIndex = random.Next(1, updatedRoute.Length - 2);
+
+                int tempNode = updatedRoute[firstIndex];
+
+                updatedRoute[firstIndex] = updatedRoute[secondIndex];
+                updatedRoute[secondIndex] = tempNode;
+            }
+
+            individual.RouteIds = updatedRoute;
+        }
+    }
+}
